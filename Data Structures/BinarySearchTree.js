1e01a82698ee58fd870a25963465aee999e7bfbc
Sandip Tajne <stajne1@gmail.com>
2019-10-22 20:15:07 +0530

    Traversing Binary Search Tree

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -50,6 +50,59 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    BFS() {
+        let visited = [];
+        if(this.root) {
+            let queue = [this.root]
+            while(queue.length) {
+                let node = queue.shift();
+                node.left && queue.push(node.left);
+                node.right && queue.push(node.right);
+                visited.push(node.value);
+            }
+        }
+        return visited;
+    }
+
+    DFSPreOrder() {
+        let visited = [];
+        if(this.root) {
+            function traverse(node) {
+                visited.push(node.value);
+                node.left && traverse(node.left);
+                node.right && traverse(node.right);
+            }
+            traverse(this.root);
+        }
+        return visited;
+    }
+
+    DFSInOrder() {
+        let visited = [];
+        if(this.root) {
+            function traverse(node) {
+                node.left && traverse(node.left);
+                visited.push(node.value);
+                node.right && traverse(node.right);
+            }
+            traverse(this.root);
+        }
+        return visited;
+    }
+
+    DFSPostOrder() {
+        let visited = [];
+        if(this.root) {
+            function traverse(node) {
+                node.left && traverse(node.left);
+                node.right && traverse(node.right);
+                visited.push(node.value);
+            }
+            traverse(this.root);
+        }
+        return visited;
+    }
 }
 
 export default BinarySearchTree;
